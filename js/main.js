53cd862577946abfb31b0b95281bb38d3e5148ca
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-08-25 10:24:19 +0500

    Add responsibility navbar to site-header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-const elLoader = document.querySelector('.lds-ripple-wrapper');
+const elLoader = document.querySelector('.lds-ripple-wrapper'),
+elNav = document.querySelector('.js-header'),
+elButton = document.querySelector('.js-menu'),
+elOpen = document.querySelector('.menu__img-open'),
+elClose = document.querySelector('.menu__img-close');
 
 
 // Loader
@@ -7,3 +11,17 @@ document.addEventListener('DOMContentLoaded', function (){
     elLoader.classList.add('lds-ripple-wrapper--none');
   }, 800);
 });
+
+
+// Navbar
+if (elButton) {
+  elButton.addEventListener('click', function (){
+    elNav.classList.toggle('sitenav__inner--open');
+    document.body.classList.toggle('body-overflow');
+    elOpen.classList
+    .toggle('menu__img-open--block')
+    elClose.classList
+    .toggle('menu__img-close--block')
+  });
+
+}
